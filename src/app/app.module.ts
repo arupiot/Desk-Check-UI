0fab7f64003c13a17b04ccb7519058efe7ed84fc
Anushan Kirupakaran <Anushan.Kirupakaran@arup.com>
2020-01-31 16:06:01 +0000

    #1 created file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { MapComponent } from './pages/map/map.component';
+import { PageSelectComponent } from './pages/page-select/page-select.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    MapComponent,
+    PageSelectComponent
   ],
   imports: [
     BrowserModule,
