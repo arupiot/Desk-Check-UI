29c41604c40d633c35cb47facd6e76579f12346a
Anushan Kirupakaran <anushan.kirupakaran@arup.com>
2020-02-07 15:13:05 +0000

    #10 Saved all changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { MapComponent } from './pages/map/map.component';
-import { PageSelectComponent } from './pages/page-select/page-select.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {
@@ -15,9 +12,6 @@ import {
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
-    MapComponent,
-    PageSelectComponent
   ],
   imports: [
     BrowserModule,
--- src/app/map/map.module.ts
+++ src/app/map/map.module.ts
@@ -3,13 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FilterComponent } from './components/filter/filter.component';
 import { FloorplanComponent } from './components/floorplan/floorplan.component';
 import { MapPageComponent } from './containers/map-page/map-page.component';
-import { ContainersComponent } from './page-select/containers/containers.component';
-import { PageSelectPageComponent } from './page-select/containers/page-select-page/page-select-page.component';
 
 
 
 @NgModule({
-  declarations: [FilterComponent, FloorplanComponent, MapPageComponent, ContainersComponent, PageSelectPageComponent],
+  declarations: [FilterComponent, FloorplanComponent, MapPageComponent],
   imports: [
     CommonModule
   ]
