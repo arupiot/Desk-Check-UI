aeb13bfc955f2f73b7a93e0589135b3c7dbdf9fd
Rory Webber <Rory.Webber@arup.com>
2020-02-17 16:49:26 +0000

    #15 updated arup logo image png to a svg component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule } from '@angular/material/button';
 import { HeaderComponent } from './core/components/header/header.component';
+import { ArupLogoComponent } from './core/components/arup-logo/arup-logo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    ArupLogoComponent,
   ],
   imports: [
     BrowserModule,
