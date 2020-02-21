4f9e6da15d2bac36805781488b8d53d2ea24e5fe
Anushan Kirupakaran <anushan.kirupakaran@arup.com>
2020-02-21 17:00:22 +0000

    #22 Created basic filters

--- src/app/map/components/filter/filter.component.ts
+++ src/app/map/components/filter/filter.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-filter',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter.component.scss']
 })
 export class FilterComponent implements OnInit {
+  brandControl = new FormControl('', [Validators.required]);
+  selectFormControl = new FormControl('', Validators.required);
+  brands: Brand[] = [
+    { value: 'Louis Vuitton', viewValue: 'Louis Vuitton'},
+    { value: 'Gucci', viewValue: 'Gucci'},
+    { value: 'Prada', viewValue: 'Prada' },
+    { value: 'Chanel', viewValue: 'Chanel' },
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+export interface Brand {
+  value: String;
+  viewValue: string;
 }
