549d3a37722f366919bc7961b5040c2a642d1f27
Anushan Kirupakaran <anushan.kirupakaran@arup.com>
2020-03-02 15:42:05 +0000

    #22 Added filters and created isFM check

--- src/app/map/components/filter/filter.component.ts
+++ src/app/map/components/filter/filter.component.ts
@@ -10,13 +10,25 @@ import {FormControl, Validators} from '@angular/forms';
 export class FilterComponent implements OnInit {
   brandControl = new FormControl('', [Validators.required]);
   selectFormControl = new FormControl('', Validators.required);
-  brands: Brand[] = [
-    { value: 'Louis Vuitton', viewValue: 'Louis Vuitton'},
-    { value: 'Gucci', viewValue: 'Gucci'},
-    { value: 'Prada', viewValue: 'Prada' },
-    { value: 'Chanel', viewValue: 'Chanel' },
+  floors: Floor[] = [
+    { value: 1, viewValue: 1},
+    { value: 2, viewValue: 2},
+    { value: 3, viewValue: 3 },
+    { value: 4, viewValue: 4},
+    { value: 5, viewValue: 5},
   ];
+  temperatures: temperature[] = [
+    { value: true, viewValue: "On"},
+    { value: false, viewValue: "Off"},
+  ];
+  selectedFloorValue = undefined;
+  selectedTempValue = undefined;
+  selectedCO2Value = undefined;
+  selectedNoiseValue = undefined;
+  selected = undefined;
+  isFM: Boolean=true;
 
+  
   constructor() { }
 
   ngOnInit() {
@@ -24,7 +36,11 @@ export class FilterComponent implements OnInit {
 
 }
 
-export interface Brand {
-  value: String;
+export interface Floor {
+  value: number;
+  viewValue: number;
+}
+export interface temperature {
+  value: Boolean;
   viewValue: string;
 }
