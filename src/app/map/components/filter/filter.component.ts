89f7894104b8268c2b48ea7140f41b489d05d985
Anushan Kirupakaran <Anushan.Kirupakaran@arup.com>
2020-03-13 15:51:26 +0000

    #22 Created new files for models

--- src/app/map/components/filter/filter.component.ts
+++ src/app/map/components/filter/filter.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
 
+import { OnOff } from '../../models/OnOff.model';
+import {Floor} from '../../models/floor.model';
+
 
 @Component({
   selector: 'app-filter',
@@ -48,11 +51,5 @@ export class FilterComponent implements OnInit {
 
 }
 
-export interface Floor {
-  value: number;
-  viewValue: number;
-}
-export interface OnOff {
-  value: Boolean;
-  viewValue: string;
-}
+
+
---
+++ src/app/map/models/floor.model.ts
@@ -0,0 +1,4 @@
+export interface Floor {
+    value: number;
+    viewValue: number;
+  }
